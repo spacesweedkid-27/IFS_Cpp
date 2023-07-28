570584df07df6ddc3529409815c2abc1cab456eb
Henri Heyden <henri@heyden5.de>
2023-07-28 14:26:06 +0200

    Updated the parser to make fitting easier and added scaling.

--- src/bin/ParseFromCpp.java
+++ src/bin/ParseFromCpp.java
@@ -10,21 +10,26 @@ import java.util.Scanner;
 
 public class ParseFromCpp {
     // 2 = 4k
-    private static final int SCALE = 4;
+    private static final int RESOLUTION = 4;
     // Make it fit your screen
-    private static final int HEIGHT = 1440 * SCALE;
+    private static final int HEIGHT = 1440 * RESOLUTION;
     // Make it fit your screen
-    private static final int WIDTH = 2560 * SCALE;
+    private static final int WIDTH = 2560 * RESOLUTION;
 
     // Image as int[][] with rgb values
-    private static int[][] image = new int[HEIGHT][WIDTH];
+    private static final int[][] image = new int[HEIGHT][WIDTH];
 
     public static void main(String[] args) throws IOException {
         // Readjust the crown
-        if (args.length != 2) {
-            System.out.println("Usage: java ParseFromCpp <path/to/plot.csv> <path/to/image.png>");
+        if (args.length != 5) {
+            System.out.println("Usage: java ParseFromCpp <path/to/plot.csv> <path/to/image.png> <X-Fit> <Y-Fit> <Scale>");
+            //System.out.println("(X-Fit and Y-Fit transpose the image they should be notated as doubles)");
             System.exit(1);
         }
+
+        double x_fit = Double.parseDouble(args[2]);
+        double y_fit = Double.parseDouble(args[3]);
+        double scale = Double.parseDouble(args[4]);
 
         // Initialise as tabula rasa
         for (int i = 0; i < HEIGHT; i++) {
@@ -46,13 +51,13 @@ public class ParseFromCpp {
             String[] split = line.split("\t");
 
             // Parse the "real" values
-            double in_x = Double.parseDouble(split[0]);
-            double in_y = Double.parseDouble(split[1]);
+            double in_x = Double.parseDouble(split[0]) * scale;
+            double in_y = Double.parseDouble(split[1]) * scale;
 
             // Now adjust it to the screen.
             // Note: 250 works really well for the output of the cpp test.
-            int x = (int)(250 * SCALE * in_x + HEIGHT/2);
-            int y = (int)(250 * SCALE * in_y);
+            int x = (int)(250 * RESOLUTION * in_x + HEIGHT/2 + x_fit * HEIGHT);
+            int y = (int)(250 * RESOLUTION * in_y + WIDTH/2 + y_fit * WIDTH);
 
             // Implement some shading by only making everything darker
             // This makes the resulting image more beautiful as it isn't as sharp.
