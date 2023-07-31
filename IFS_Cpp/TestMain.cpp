e53215cf09d39f6ae42ef1c896ec7e49d9e59026
Henri Heyden <henri@heyden5.de>
2023-07-31 02:17:19 +0200

    Massive commit: -Added an integrated plotter. -Speed-up of ~1000% compared to normal data export -Usage of ppm format -Less filesize from plot (not perfect since there is no compression) -Dataexport and usage of the Java-Plotter is now deprecated and will be deleted in the next update

--- IFS_Cpp/TestMain.cpp
+++ IFS_Cpp/TestMain.cpp
@@ -30,15 +30,240 @@ namespace examples
 	function_collection default_ = function_collection(default__, 4, 0);
 }
 
-// I am ok with typing std:: always but examples:: is to long for me.
-using namespace examples;
 
-constexpr char usage[] = "Usage: IFS.Cpp.exe <random / default> [?random: <GEN_SEED> <length>] <START_SEED> <append?>\n(START_SEED is used for the actual calculation of the data while GEN_SEED is used by the random function_collection to determine it's values)";
+// --help
+constexpr char usage[] = "IFS.Cpp.exe <random / default> [?random: <GEN_SEED> <length>] <START_SEED> <append?> [?!append <plot directly?> [?plot directly <x-fit> <y-fit> <scale>]]\n(START_SEED is used for the actual calculation of the data while GEN_SEED is used by the random function_collection to determine it's values)";
 
-int main(int argc,char *argv[])
+// This number works well for 2*4k resolution when plotting it.
+constexpr unsigned long long MAX = 20000000;
+
+unsigned int bin(const char in[])
+{
+	return std::stoi(in, nullptr, 2);
+}
+
+
+int main(int argc, char *argv[])
+{
+	//std::cout << "test" << std::endl;
+	
+	// USAGE SHOULD BE:
+	// IFS.Cpp.exe <random / default> [?random: <GEN_SEED> <length>] <START_SEED> <append?> [?!append <plot directly?> [?plot directly <x-fit> <y-fit> <scale>]]
+	// So valid argc are 4 or 8 for default mode and 6 or 12 for random mode.
+	// (if we want to plot directly, we have to give the parameters x-fit y-fit and scale)
+
+	unsigned long start_seed;
+	unsigned long gen_seed;
+	unsigned long length;
+	bool append;
+	bool plot_directly = false;
+
+	constexpr double RESOLUTION = 1;
+	constexpr int HEIGHT = 1440 * RESOLUTION;
+	constexpr int WIDTH = 2560 * RESOLUTION;
+
+
+	int xFit = 0;
+	int yFit = 0;
+	int scale = 1;
+
+	
+	if (argc == 4 || argc == 5 || argc == 8)
+	{
+		// default mode
+		if (argv[1] != std::string("default"))
+		{
+			std::cout << usage << std::endl;
+			return 1;
+		}
+
+		start_seed = std::stoul(argv[2] , nullptr, 0);
+		examples::default_.SEED = start_seed;
+
+		append = (argv[3] == std::string("true"));
+
+		if (!append)
+		{
+			plot_directly = (argv[4] == std::string("true"));
+			if (plot_directly)
+			{
+				xFit = std::stoul(argv[5], nullptr, 0);
+				yFit = std::stoul(argv[6], nullptr, 0);
+				scale = std::stoul(argv[7], nullptr, 0);
+			}
+		}
+	} else if (argc == 6 || argc == 7 || argc == 10)
+	{
+		// random mode
+		if (argv[1] != std::string("random"))
+		{
+			std::cout << usage << std::endl;
+			return 1;
+		}
+
+		gen_seed = std::stoul(argv[2], nullptr, 0);
+		length = std::stoul(argv[3], nullptr, 0);
+		start_seed = std::stoul(argv[4], nullptr, 0);
+		append = (argv[5] == std::string("true"));
+
+		if (!append)
+		{
+			plot_directly = (argv[6] == std::string("true"));
+			if (plot_directly)
+			{
+				xFit = std::stoul(argv[7], nullptr, 0);
+				yFit = std::stoul(argv[8], nullptr, 0);
+				scale = std::stoul(argv[9], nullptr, 0);
+			}
+		}
+
+		examples::default_ = function_collection(length, start_seed, gen_seed);
+
+	} else
+	{
+		std::cout << usage << std::endl;
+		return 1;
+	}
+
+	std::cout << "Starting calculation of " << argv[1] << " IFS plot." << std::endl;
+
+	if (plot_directly)
+	{
+		// Initialize the positions
+		double pos_x = 0.0;
+		double pos_y = 0.0;
+
+		// and the last positions.
+		// we have to use them to avoid collision between calculate_x and calculate_y (x gets overwritten).
+		double x = pos_x;
+		double y = pos_y;
+
+		int ix = 0;
+		int iy = 0;
+
+		auto (*image)[WIDTH] = new unsigned char[HEIGHT][WIDTH]{};
+
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				image[i][j] = static_cast<unsigned char>(bin("11111111"));
+			}
+		}
+
+		// Stop after MAX iterations
+		for (unsigned long long runner = 0; runner < MAX; runner++)
+		{
+			// Calculate the next positions
+			pos_x = examples::default_.calculate_x(x, y);
+			pos_y = examples::default_.calculate_y(x, y);
+
+			// Pick the next function.
+			examples::default_.pick_own_index();
+
+			// old -> new
+			x = pos_x;
+			y = pos_y;
+
+			ix = static_cast<int>(250 * RESOLUTION * (x * scale) + WIDTH / 2 + xFit * WIDTH);
+			iy = static_cast<int>(250 * RESOLUTION * (y * scale) + HEIGHT / 2 + yFit * HEIGHT);
+
+			if (0 <= ix && ix < WIDTH && 0 <= iy && iy < HEIGHT)
+			{
+				image[iy][ix] = static_cast<unsigned char>(0.99 * static_cast<unsigned short>(image[iy][ix]));
+			}
+
+			//std::cout << ix << " " << iy << std::endl;
+
+			// Makes it a bit slower, but you get a progress so that you can pick better MAX values.
+			if (runner % (MAX / 100) == 0)
+			{
+				std::cout << "Progress: " << static_cast<float>(runner) * 100 / static_cast<float>(MAX) << "%" << std::endl;
+			}
+		}
+
+		std::cout << "Plotting directly..." << std::endl;
+
+		std::ofstream plot1;
+		plot1.open("plot.ppm", std::ios::out | std::ios::trunc);
+		plot1 << "P2\n" << WIDTH << " " << HEIGHT << "\n255" << std::endl;
+		
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+
+				plot1 << static_cast<unsigned short>(image[i][j]) << " ";
+				//plot1 << (image[i][j] & bin("111111110000000000000000") >> 16) << " ";
+				//plot1 << (image[i][j] & bin("1111111100000000") >> 8) << " ";
+				//plot1 << (image[i][j] & bin("11111111")) << " ";
+			}
+			plot1 << "\n";
+		}
+
+		// Delete the values to free RAM.
+		delete[] image;
+
+		plot1.close();
+
+		std::cout << "Plot saved as \"plot.ppm\"" << std::endl;
+
+	} else
+	{
+		std::cout << "Calculating plot data and saving to \"plot.csv\"" << std::endl;
+
+		// Write into this file which we'll name "plot.csv"
+		std::ofstream plot;
+		// open the file, if it already exists, overwrite it.
+		if (!append)
+			plot.open("plot.csv", std::ios::out | std::ios::trunc);
+		else
+			plot.open("plot.csv", std::ios::out | std::ios::app);
+
+		// Initialize the positions
+		double pos_x = 0.0;
+		double pos_y = 0.0;
+
+		// and the last positions.
+		// we have to use them to avoid collision between calculate_x and calculate_y (x gets overwritten).
+		double x = pos_x;
+		double y = pos_y;
+
+		// Stop after MAX iterations
+		for (unsigned long long runner = 0; runner < MAX; runner++)
+		{
+			// Calculate the next positions
+			pos_x = examples::default_.calculate_x(x, y);
+			pos_y = examples::default_.calculate_y(x, y);
+
+			// Pick the next function.
+			examples::default_.pick_own_index();
+
+			// old -> new
+			x = pos_x;
+			y = pos_y;
+
+			// add to plot.csv
+			plot << pos_x << "\t" << pos_y << std::endl;
+
+			// Makes it a bit slower, but you get a progress so that you can pick better MAX values.
+			if (runner % (MAX / 100) == 0)
+			{
+				std::cout << "Progress: " << static_cast<float>(runner) * 100 / static_cast<float>(MAX) << "%" << std::endl;
+			}
+		}
+		// close the file
+		plot.close();
+		std::cout << "Saved all plot data as \"plot.csv\"" << std::endl;
+	}
+
+	std::cout << "Finished." << std::endl;
+
+	return 0;
+	
+
+}
+
+/*
+int old_main(int argc,char *argv[])
 {
 	// From here to line 54 we just check what the user wants.
-	// TODO Implement Seeds from command line.
 
 	// The argc should be in [3, 5] (we don't count the path to the executable)
 
@@ -72,7 +297,7 @@ int main(int argc,char *argv[])
 		std::cout << "starting random pattern with generative seed: " << gen_seed << " and start seed: " << start_seed << std::endl;
 
 
-	} else if (!std::string(argv[1])._Equal("default") || argc != 4)
+	} else if (!std::string(argv[1])._Equal("default"))
 	{
 		std::cout << usage << std::endl;
 		return 1;
@@ -137,3 +362,4 @@ int main(int argc,char *argv[])
 	// close the file
 	plot.close();
 }
+*/
